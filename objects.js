5f0c3a8f69a14efa49c7b19ce2785e9bfc66a548
Harrison Barker <harrison@thebarkers.me.uk>
2018-03-01 22:31:46 +0000

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,42 @@
+var playerOne = {
+        position : {
+            x : 25, 
+            y : 25
+        },
+        velocity : {
+            x : 0,
+            y : 0
+        },
+        size : {
+            height : 50,
+            width : 14
+        },
+        onGround : false,
+        exitVelocity : 10
+    }
+
+var basicBlock = {
+    position: {
+        x : null,
+        y : null
+    },
+    size : {
+        height : 50,
+        width : 14
+    }
+}
+
+var movingBlock = {
+    position: {
+        x : null,
+        y : null
+    },
+    velocity : {
+        x : 0,
+        y : 0
+    },
+    size : {
+        height : 50,
+        width : 14
+    }
+}
