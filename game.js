0ef83975043466904d69317c81b5519cba32c1a4
Ronnie Barker <ronnie.barker@theagileworkshop.com>
2018-03-04 20:10:37 +0000

    Almost a game!

--- game.js
+++ game.js
@@ -2,6 +2,7 @@ function drawGame()
 {
 	context.clearRect(0,0,width,height);
 	players.forEach( drawPlayer );
+	things.forEach( drawThing );
 	drawLevel();
 }
 
@@ -10,7 +11,20 @@ function drawGame()
 
 function drawPlatform( p )
 {
-	makeRectangle( p.Position.X, p.Position.Y, p.Size.Width, p.Size.Height, "brown" );
+	// makeRectangle( p.Position.X, p.Position.Y, p.Size.Width, p.Size.Height, "yellow" );
+	DrawLines( p.Lines, "red" );
+}
+
+
+
+function drawThing( t )
+{
+    context.beginPath();
+    context.arc( t.Position.X , t.Position.Y , t.Size.Width/2, 0, 2*Math.PI );
+    context.fillStyle = "blue";
+    context.fill();
+    context.closePath();
+	// makeRectangle( t.Position.X, t.Position.Y, t.Size.Width, t.Size.Height, "blue" );
 }
 
 
@@ -26,8 +40,44 @@ function drawLevel()
 
 function drawPlayer( player )
 {
-    makeRectangle(player.Position.X, player.Position.Y, player.Size.Width, player.Size.Height, "violet");
+	// makeRectangle(player.Position.X, player.Position.Y, player.Size.Width, player.Size.Height, "violet");
+	DrawLines( player.Lines, "blue" );
 }
+
+
+
+
+function DrawLines( lines, colour )
+{
+	lines.forEach( l =>
+	{
+		context.beginPath();
+		context.moveTo( l.Start.X, l.Start.Y );
+		context.lineTo( l.End.X, l.End.Y );
+		if( l.I )
+		{
+			context.strokeStyle = "red";
+		}
+		else
+		{
+			context.strokeStyle = "green";
+		}
+		context.stroke();
+
+		var n = l.Vector.Normal;
+		var midX = ( l.Start.X + l.End.X ) / 2;
+		var midY = ( l.Start.Y + l.End.Y ) / 2;
+
+		// context.beginPath();
+		// context.moveTo( midX, midY );
+		// context.lineTo( midX + ( n.X * 30 ) , midY + ( n.Y * 30 ) );
+		// context.strokeStyle = "pink";
+		// context.lineCap = "round";
+		// context.lineWidth = 5;
+		// context.stroke();
+	} );
+}
+
 
 
 
@@ -51,26 +101,32 @@ window.onkeydown = function(e) { keys[e.keyCode] = true; }
 
 function processKeys()
 {
-
+	var thrust = 2;
 	if( keys[ 37 ] )
 	{
-		players[0].Position.x -= 10;
+		players[0].velocity.x -= thrust;
 	}
-
-	if( keys[ 39 ] )
+	else if( keys[ 39 ] )
 	{
-		players[0].Position.x += 10;
+		players[0].velocity.x += thrust;
 	}
+	// else
+	// {
+	// 	players[0].Velocity.x = 0;
+	// }
 
 	if( keys[ 38 ] )
 	{
-		players[0].Position.y -= 10;
+		players[0].velocity.y += thrust * -3;
 	}
-
-	if( keys[ 40 ] )
-	{
-		players[0].Position.y += 10;
-	}
+	// else if( keys[ 40 ] )
+	// {
+	// 	players[0].velocity.y = thrust * 1;
+	// }
+	// else
+	// {
+	// 	players[0].Velocity.y = 0;
+	// }
 
 	if( players[0].Position.x < 0 )
 	{
@@ -90,34 +146,59 @@ function processKeys()
 function checkPosition()
 {
 	//TODO: Some refactoring and movin into the classes
-	players.forEach( o =>
+	allObjects.forEach( o =>
 	{
-		if( o.Position.X > ( width - o.Size.Width ) )
+		if( o instanceof MovingBlock )
 		{
-			o.Position.x = width -  o.Size.Width;
-		}
+			// if( o.Position.X > ( width - o.Size.Width ) )
+			// {
+			// 	o.Position.x = width -  o.Size.Width;
+			// }
 
-		platforms.forEach( p =>
-		{
-			if(
-				( o.Position.X > p.Position.X )
-				&&
-				( o.Position.X < ( p.Position.X + p.Size.Width ) )
-				&&
-				( o.Bottom > p.Position.Y )
-				&&
-				( o.Bottom < p.Bottom )
-				)
+			allObjects.forEach( p =>
 			{
-				o.Position.y = p.Position.Y - o.Size.Height;
-				o.Velocity.y = -o.Velocity.Y * 0.4;
-
-				if( Math.abs( o.Velocity.Y ) < 20 )
+				if( ! Object.is( p, o ) )
 				{
-					o.Velocity.y = 0;
+				var lines = p.Intersects( o );
+				if( lines.length > 0 )
+				{
+					o.ResetPosition();
+
+					var n = lines[0].Vector.Normal;
+					var dot = DotProduct( o.Velocity, n )
+					if( o.Mass > 0 )
+					{
+						dot *= 0.8;
+						if( Math.abs( dot ) < 20 )
+						{
+							// dot = 0;
+							o.velocity = new Velocity( 0, 0 );
+						}
+						else
+						{
+							o.velocity = n.Times( dot * -2).Add( o.Velocity );
+						}
+					}
+					else
+					{
+						o.velocity = n.Times( dot * -2).Add( o.Velocity );
+					}
+
+
+
+	//				o.Position.y = p.Position.Y - o.Size.Height;
+					// o.Velocity.y = o.Velocity.Y * ( ( o.Mass * 0.6 ) - 1 );
+					// o.Velocity.x = -o.Velocity.X * 0.4;
+
+					// if( o.Mass > 0 && Math.abs( o.Velocity.Y ) < 20 )
+					// {
+					// 	o.Velocity.y = 0;
+					// }
 				}
-			}
-		} );
+
+			} });
+			o.AcceptPosition();
+		}
 	} );
 }
 
@@ -138,6 +219,12 @@ var oldTicks = getTicks();
 
 function gameLoop()
 {
+	allObjects.forEach( o => {
+		o.Lines.forEach( l => {
+			l.I = false
+		});
+	 } );
+
 	var ticks = getTicks();
 	processKeys();
 	processPhysics( oldTicks, ticks )
@@ -152,7 +239,7 @@ function gameLoop()
 
 function processPhysics( oldTicks, ticks )
 {
-	var deltaT = ( ticks - oldTicks ) / 50;
+	var deltaT = ( ticks - oldTicks ) / 100;
 
 	allObjects.forEach( o =>
 	{
@@ -161,7 +248,10 @@ function processPhysics( oldTicks, ticks )
 			o.Position.x += o.Velocity.X * deltaT;
 			o.Position.y += o.Velocity.Y * deltaT;
 
-			o.Velocity.y += gravity * deltaT;
+			if( o.Mass > 0 )
+			{
+				o.Velocity.y += gravity * deltaT;
+			}
 		}
 	} );
 }
--- objects.js
+++ objects.js
@@ -16,7 +16,33 @@ class Vector
     get Y()
     {
         return this.y;
-    }
+	}
+
+
+	get Modulus()
+	{
+		return Math.sqrt( ( this.x * this.x ) + ( this.y * this.y ) );
+	}
+
+
+	get Normal()
+	{
+		var m = this.Modulus;
+		return new Vector( this.y / m, -this.x / m );
+	}
+
+
+	Times( a )
+	{
+		return new Vector( this.X * a, this.Y * a );
+	}
+
+
+	Add( v )
+	{
+		return new Vector( this.X + v.X, this.Y + v.Y );
+	}
+
 }
 
 
@@ -28,6 +54,118 @@ class Position extends Vector
     {
         super( x, y );
     }
+}
+
+
+
+class Line
+{
+    constructor( startPosition, endPosition )
+    {
+        this.start = startPosition;
+		this.end = endPosition;
+		this.I = true;
+    }
+
+
+    get Start()
+    {
+        return this.start;
+    }
+
+
+    get End()
+    {
+        return this.end;
+	}
+
+
+	get BoundingBox()
+	{
+		var minX = Math.min( this.Start.X, this.End.X );
+		var maxX = Math.max( this.Start.X, this.End.X );
+		var minY = Math.min( this.Start.Y, this.End.Y );
+		var maxY = Math.max( this.Start.Y, this.End.Y );
+
+		return new Line( new Position( minX, minY ), new Position( maxX, maxY ) );
+	}
+
+
+	get Vector()
+	{
+		return new Vector( this.End.X - this.Start.X, this.End.Y - this.Start.Y );
+	}
+}
+
+
+
+
+EPSILON = 0.000001;
+
+function CrossProduct( /*Position*/ a, /*Position*/ b )
+{
+    return a.X * b.Y - b.X * a.Y;
+}
+
+
+
+function DotProduct( /*Vector*/ a, /*Vector*/ b )
+{
+	return ( a.X * b.X ) + ( a.Y * b.Y );
+}
+
+
+
+function IsPointOnLine( lineA, positionB)
+{
+    // Move the image, so that a.first is on (0|0)
+    var lineATmp = new Line( new Position( 0, 0 ), new Position( lineA.End.X - lineA.Start.X, lineA.End.Y - lineA.Start.Y ) );
+    var positionBTmp = new Position( positionB.X - lineA.Start.X, positionB.Y - lineA.Start.Y );
+
+    var r = CrossProduct( lineATmp.End, positionBTmp );
+
+    return Math.abs( r ) < EPSILON;
+}
+
+
+function IsPointRightOfLine( lineA, positionB)
+{
+    // Move the image, so that a.first is on (0|0)
+    lineATmp = new Line( new Position( 0, 0 ), new Position( lineA.End.X - lineA.Start.X, lineA.End.Y - lineA.Start.Y ) );
+    positionBTmp = new Position( positionB.X - lineA.Start.X, positionB.Y - lineA.Start.Y );
+    return CrossProduct( lineATmp.End, positionBTmp ) < 0;
+}
+
+
+function DoesLineSegmentTouchOrCrossLine( lineA, lineB )
+{
+    return IsPointOnLine( lineA, lineB.Start )
+        || IsPointOnLine( lineA, lineB.End )
+        || ( IsPointRightOfLine( lineA, lineB.Start ) ^ IsPointRightOfLine( lineA, lineB.End ) );
+}
+
+
+function DoBoundingBoxesIntersect( lineA, lineB )
+{
+	var aBox = lineA.BoundingBox;
+	var bBox = lineB.BoundingBox;
+
+	var a = aBox.Start.X <= bBox.End.X;
+	var b = bBox.Start.X <= aBox.End.X;
+	var c = aBox.Start.Y <= bBox.End.Y;
+	var d = bBox.Start.Y <= aBox.End.Y;
+
+	return a && b && c && d;
+}
+
+
+function DoLinesIntersect( lineA, lineB )
+{
+	var a = DoBoundingBoxesIntersect( lineA, lineB );
+	var b = DoesLineSegmentTouchOrCrossLine( lineA, lineB );
+	var c = DoesLineSegmentTouchOrCrossLine( lineB, lineA );
+
+	return a && b && c;
 }
 
 
@@ -103,12 +241,48 @@ class Acceleration extends Vector
 
 
 
+
 class BasicBlock
 {
-    constructor( initialPosition, size )
+    Intersects( otherBasicBlock )
     {
+        var lines = this.Lines;
+		var otherLines = otherBasicBlock.Lines;
+
+		var result = [];
+
+        for( var i=0; i<lines.length; i++ )
+        {
+            var line = lines[ i ];
+
+            for( var j=0; j<otherLines.length; j++ )
+            {
+                var otherLine = otherLines[ j ];
+
+				if( DoLinesIntersect( line, otherLine ) )
+				{
+					line.I = true;
+					otherLine.I = true;
+
+					result.push( line );
+				}
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+class RectangleBlock extends BasicBlock
+{
+	constructor( initialPosition, size )
+    {
+		super();
         this.position = initialPosition;
-        this.size = size;
+		this.size = size;
+		this.lines = this.MakeLines();
     }
 
 
@@ -121,31 +295,118 @@ class BasicBlock
     get Size()
     {
         return this.size;
-    }
+	}
+
+
 
 
     get Bottom()
     {
         return this.Position.Y + this.Size.Height;
+	}
+
+
+	get Lines()
+	{
+		var l = this.MakeLines();
+		for( var i=0; i<l.length; i++ )
+		{
+			this.lines[i].start = l[i].start;
+			this.lines[i].end = l[i].end;
+		}
+		return this.lines;
+	}
+
+
+    MakeLines()
+    {
+        var left = this.Position.X;
+        var right = left + this.Size.Width;
+        var top = this.Position.Y;
+        var bottom = top + this.Size.Height;
+
+        var tl = new Position( left, top );
+        var tr = new Position( right, top );
+        var bl = new Position( left, bottom );
+        var br = new Position( right, bottom );
+
+        return [
+            new Line( tl, tr ),
+            new Line( tr, br ),
+            new Line( br, bl ),
+            new Line( bl, tl ),
+        ];
     }
 }
 
 
 
+class LinesBlock extends BasicBlock
+{
+	constructor( positions )
+	{
+		super();
+		this.lines = [];
+		for( var i = 1; i < positions.length; i++ )
+		{
+			this.lines.push( new Line( positions[ i ], positions[ i - 1 ] ) );
+		}
 
-class MovingBlock extends BasicBlock
+		this.lines.push( new Line( positions[ 0 ], positions[ positions.length - 1 ] ) );
+	}
+
+
+	get Lines()
+	{
+		return this.lines;
+	}
+}
+
+
+
+
+
+
+
+class MovingBlock extends RectangleBlock
 {
     constructor( initialPosition, size, initialVelocity )
     {
         super( initialPosition, size );
         this.velocity = initialVelocity;
+
+		this.oldPosition = new Position( initialPosition.X, initialPosition.Y );
     }
 
 
     get Velocity()
     {
         return this.velocity;
-    }
+	}
+
+
+	get Mass()
+	{
+		return 1;
+	}
+
+
+	AcceptPosition()
+	{
+		this.oldPosition.x = this.position.X;
+		this.oldPosition.y = this.position.Y;
+	}
+
+
+	ResetPosition()
+	{
+		if( this.position.Y != this.oldPosition.Y )
+		{
+			// alert("here");
+		}
+		this.position.x = this.oldPosition.X;
+		this.position.y = this.oldPosition.Y;
+	}
 }
 
 
@@ -155,24 +416,50 @@ class Player extends MovingBlock
 {
     constructor()
     {
-        super( new Position( 25, 700 ), new Size( 14, 50 ), new Velocity( 0, 0 ) );
+		super( new Position( 25, 700 ), new Size( 14, 50 ), new Velocity( 0, 0 ) );
     }
 }
 
 
 
+class Thing extends MovingBlock
+{
+	constructor( initialPosition )
+	{
+		super( initialPosition, new Size( 14, 14 ), new Velocity( 10, 10 ) );
+	}
+
+
+	get Mass()
+	{
+		return 0;
+	}
+}
+
 
 
 var players = [ new Player() ];
 
+var things = [ ];
+for( var i=0; i < Math.random() * 10; i++ )
+{
+	things.push( new Thing( new Position( Math.random() * 500, Math.random() * 500 ) ) );
+}
 
 var platforms =
 [
-    new BasicBlock( new Position( 0, 870 ), new Size( 900, 30 ) ),
-    new BasicBlock( new Position( 50, 500 ), new Size( 400, 30 ) ),
-    new BasicBlock( new Position( 300, 800 ), new Size( 400, 30 ) ),
-    new BasicBlock( new Position( 400, 700 ), new Size( 400, 30 ) ),
-    new BasicBlock( new Position( 500, 600 ), new Size( 400, 30 ) ),
+	new RectangleBlock( new Position( -100, 890 ), new Size( 1200, 100 ) ),
+	new RectangleBlock( new Position( -100, -90 ), new Size( 1200, 100 ) ),
+
+	new RectangleBlock( new Position( -90, -100 ), new Size( 100, 1200 ) ),
+	new RectangleBlock( new Position( 890, -100 ), new Size( 100, 1200 ) ),
+
+	new RectangleBlock( new Position( 50, 500 ), new Size( 400, 30 ) ),
+    new RectangleBlock( new Position( 300, 800 ), new Size( 400, 30 ) ),
+    new RectangleBlock( new Position( 400, 700 ), new Size( 400, 30 ) ),
+	new RectangleBlock( new Position( 500, 600 ), new Size( 400, 30 ) ),
+
+	new LinesBlock( [ new Position( 700, 500 ), new Position( 800, 450 ), new Position( 900, 200 ), new Position( 600, 50 ) ] ),
 ]
 
 
@@ -180,3 +467,4 @@ var allObjects = [];
 
 players.forEach( x => allObjects.push( x ) );
 platforms.forEach( x => allObjects.push( x ) );
+things.forEach( x => allObjects.push( x ) );
